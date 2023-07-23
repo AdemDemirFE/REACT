720e3b388289b3aeb5a2886281da60caea7f3ee7
Adem Demir <93316402+AdemDemirFE@users.noreply.github.com>
2023-07-23 16:46:55 +0300

    Prob drilling

--- reactapp/src/components/User.js
+++ reactapp/src/components/User.js
@@ -5,35 +5,47 @@ import { faFacebookF } from "@fortawesome/free-brands-svg-icons";
 import { faCheckSquare, faCoffee } from "@fortawesome/free-solid-svg-icons";
 
 class User extends Component {
-  static defaultProps = {
-    name: "Bilgi Yok",
-    salary: "Bilgi Yok",
-    department: "Bilgi Yok",
-  };
+    state = {
+        isVisible : false
+    }
+    static defaultProps = {
+        name: "Bilgi Yok",
+        salary: "Bilgi Yok",
+        department: "Bilgi Yok",
+    };
 
-  render() {
-    const { name, department, salary } = this.props;
-    return (
-      <div className="col-md-8 mb-4">
-        <div className="card">
-          <div className="card-header d-flex justify-content-between align-items-center">
-            <h4 className="d-inline">{name}</h4>
-            <i className="far fa-trash-alt" style={{ cursor: "pointer" }}></i>
-          </div>
-          <div className="card-body">
-            <p className="card-text">Salary: {salary}</p>
-            <p className="card-text">Department: {department}</p>
-          </div>
-        </div>
-      </div>
-    );
-  }
+    onClickEvent = (e) =>{
+        this.setState({
+            isVisible : ! this.state.isVisible
+        })
+ 
+    }
+    render() {
+        const { name, department, salary } = this.props;
+        const {isVisible} = this.state;
+        return (
+            <div className="col-md-8 mb-4">
+                <div className="card">
+                    <div className="card-header d-flex justify-content-between align-items-center"onClick={this.onClickEvent}>
+                        <h4 className="d-inline" >{name}</h4>
+                        <i className="far fa-trash-alt" style={{ cursor: "pointer" }}></i>
+                    </div>
+                    {
+                        isVisible ? <div className="card-body">
+                            <p className="card-text">Salary: {salary}</p>
+                            <p className="card-text">Department: {department}</p>
+                        </div> : null
+                    }
+                </div>
+            </div>
+        );
+    }
 }
 
 User.propTypes = {
-  name: PropTypes.string.isRequired,
-  salary: PropTypes.string.isRequired,
-  department: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    salary: PropTypes.string.isRequired,
+    department: PropTypes.string.isRequired,
 };
 
 export default User;
